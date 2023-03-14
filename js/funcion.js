cdcb29e8e7981221e48b2ae9bd0124808ea0f999
Sebamazu <mazunikseba@gmail.com>
2023-03-14 01:07:59 -0300

    Subida de archivos a git pages

---
+++ js/funcion.js
@@ -0,0 +1,43 @@
+
+document.getElementById('botonexpe').onclick = function(){
+    document.getElementById('laboral').style.display = 'block'
+    document.getElementById('educacion').style.display = 'block'
+}
+
+document.getElementById('boton-nombre').onmouseover = function(){
+    document.getElementById('contacto').style.display = 'block'
+}
+document.getElementById('boton-nombre').onmouseout = function(){
+    document.getElementById('contacto').style.display = 'none'
+}
+
+document.getElementById('banerr2').onmouseover = function(){
+    document.getElementById('banerr').style.color = 'white'
+}
+document.getElementById('banerr2').onmouseout = function(){
+    document.getElementById('banerr').style.color = 'black'
+}
+document.getElementById('sobre2').onmouseover = function(){
+    document.getElementById('sobre').style.color = 'white'
+}
+document.getElementById('sobre2').onmouseout = function(){
+    document.getElementById('sobre').style.color = 'black'
+}
+document.getElementById('expe2').onmouseover = function(){
+    document.getElementById('expe').style.color = 'white'
+}
+document.getElementById('expe2').onmouseout = function(){
+    document.getElementById('expe').style.color = 'black'
+}
+document.getElementById('skil2').onmouseover = function(){
+    document.getElementById('skil').style.color = 'white'
+}
+document.getElementById('skil2').onmouseout = function(){
+    document.getElementById('skil').style.color = 'black'
+}
+document.getElementById('proye2').onmouseover = function(){
+    document.getElementById('proye').style.color = 'white'
+}
+document.getElementById('proye2').onmouseout = function(){
+    document.getElementById('proye').style.color = 'black'
+}
